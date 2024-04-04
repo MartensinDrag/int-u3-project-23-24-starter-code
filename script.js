beba2752bb94d05c096e89b95ee09a81150a3211
MartensinDrag <149087916+MartensinDrag@users.noreply.github.com>
2024-04-04 15:24:48 +0000

    cool coding thinfs

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 // Declare variables below to save the different divs of your story.
-
-
-
-
+let storyOpening = document.querySelector("story-opening")
+let normalButton = document.querySelector("normal-button")
+let funkyButton = document.querySelector("funky-button")
+let normalScreen = document.querySelector("normal-screen")
+let bigButton = document.querySelector("big-button")
+let smallButton = document.querySelector("small-button")
+let funkyScreen = document.querySelector("funky-screen")
+let noLegsButton = document.querySelector("no-legs-button")
+let allLegsButton = document.querySelector("all-legs-button")
+let bigPetEnding = document.querySelector("big-pet-ending")
+let smallPetEnding = document.querySelector("small-pet-ending")
+let noLegsEnding = document.querySelector("no-legs-ending")
+let allLegsEnding = document.querySelector("all-legs-ending")
 
 
 
